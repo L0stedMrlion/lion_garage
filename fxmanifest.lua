15b940ea4ae652ff01569273a2e1bae1c007eff7
Mrlion <mrlion4202@gmail.com>
2024-03-23 16:11:16 +0000

    added some files

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 "yes"
+
+name "LionGarage"
+author 'Mrlion'
+description "Mrlion's FiveM script for garages"
+version '1.0'
+
+shared_script {
+  "@es_extended/imports.lua",
+  "@ox_lib/init.lua",
+}
+
+client_scripts {
+  "client/client.lua",
+}
