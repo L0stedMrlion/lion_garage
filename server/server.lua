1042a8211e44ce0af786a03b878330ebde45e95c
Mrlion <mrlion4202@gmail.com>
2024-03-23 19:37:48 +0000

    m,ore changes

--- server/server.lua
+++ server/server.lua
@@ -3,11 +3,11 @@ if Config.Updater then
 
         local ScriptVersion = GetResourceMetadata(GetCurrentResourceName(), "version")
     
-        if ScriptVersion == Version then
-            print("^2[INFO] LionGarage have latest version!")
-        else
+        if ScriptVersion ~= Version then
             print("^3[UPDATE] Update for LionGarage is available! ("..ScriptVersion.. " -> " ..Version.. ")")
             print("^3[UPDATE] https://github.com/L0stedMrlion/LionGarage")
+        else
+            print("^2[INFO] LionGarage have latest version!")
         end
     end)
 end
